2105454e0974954f1a76d5151a0ce53e32b344ec
damjan <joveski.damjan@uklo.edu.mk>
2023-09-28 17:42:00 +0200

    Hero section done

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,29 @@
+import {TypeAnimation} from "react-type-animation";
+
 export const Main = () => {
     return (
-        <div id='main'>
-
+        <div id='main' className='z-10'>
+            <div className='w-full h-screen'>
+                <div className='max-w-[900px] mx-auto h-full w-full py-52 px-4 lg:py-72'>
+                    <h1>
+                        <TypeAnimation
+                            sequence={[
+                                'Hello there',
+                                1000,
+                                'My name is Damjan Joveski',
+                                1000,
+                                "I'm on a journey to become a software engineer",
+                                1000,
+                                "Here's a little bit about myself and my journey...",
+                            ]}
+                            wrapper="span"
+                            speed={20}
+                            style={{ fontSize: '3em', display: 'inline-block', color:'white' }}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
--- src/ParticleBackground.jsx
+++ src/ParticleBackground.jsx
@@ -3,16 +3,10 @@ import { loadFull } from "tsparticles";
 export const ParticleBackground = () => {
 
     const particlesInit = async (main) => {
-        console.log(main);
-
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
         await loadFull(main);
     };
 
     const particlesLoaded = (container) => {
-        console.log(container);
     };
 
     return (
@@ -46,7 +40,7 @@ export const ParticleBackground = () => {
             delay: 0,
             fullScreen: {
                 enable: true,
-                zIndex: 1
+                zIndex: -1
             },
             detectRetina: true,
             duration: 0,
